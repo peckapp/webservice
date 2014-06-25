5452a66816b644a98b444bdbf3969c9ac31fddcb
Anthoney Tsou <anttsou@gmail.com>
2014-06-25 17:08:45 -0400

    Rabl Protocols

--- app/controllers/api/v1/simple_events_controller.rb
+++ app/controllers/api/v1/simple_events_controller.rb
@@ -3,19 +3,7 @@ module Api
     class SimpleEventsController < ApplicationController #Api::BaseController
 
     respond_to :json
-    # def create
-    #   @simple_event = SimpleEvent.new(params[simple_event_params])
-    #   respond_to do |format|
-    #     if @simple_event.save
-    #       format.json { render json: @simple_event, status: :created}
-    #       format.xml { render xml: @simple_event, status: :created}
-    #     else
-    #       format.json { render json: @simple_event.errors, status: :unprocessable_entity }
-    #       format.xml { render json: @simple_event.errors, status: :unprocessable_entity }
-    #     end
-    #   end
-    # end
-
+    
     def index
       @simple_events = SimpleEvent.all
     end
@@ -24,31 +12,23 @@ module Api
       @simple_event = SimpleEvent.find(params[:id])
     end
 
+    def create
+      @simple_event = SimpleEvent.create(simple_event_params)
+    end
+
     def update
-      @simple_event = SimpleEvent.find(params[:id])
-      respond_to do |format|
-        if @simple_event.update_attributes(params[simple_event_params])
-          format.json { render json: @simple_event, status: :ok}
-          format.xml { render xml: @simple_event, status: :ok}
-        else
-          format.json { render json: @simple_event.errors, status: :unprocessable_entity}
-          format.xml { render xml: @simple_event.errors, status: :unprocessable_entity}
-        end
-      end
+      @simple_event = SimpleEvent.find(params[:id]).update_attributes(simple_event_params)
     end
 
     def destroy
-      @simple_event = SimpleEvent.find(params[:id])
-      respond_to do |format|
-        if @simple_event.destroy
-          format.json { head :no_content, status: :ok}
-          format.xml { head :no_content, status: :ok}
-        else
-          format.json { render json: @simple_event.errors, status: :unprocessable_entity}
-          format.xml { render xml: @simple_event.errors, status: :unprocessable_entity}
-        end
-      end
+      @simple_event = SimpleEvent.find(params[:id]).destroy
     end
+
+    private
+
+    def simple_event_params
+      params.require(:simple_event).permit(:title, :event_description, :institution_id, :user_id, :department_id, :club_id, :circle_id, :event_url, :open, :image_url, :comment_count, :start_date, :end_date)
     end
   end
+end
 end
