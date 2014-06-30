5832a96d6cd3c517ec089a1450c58f91f0bf55fc
JDrobro <jsd1@williams.edu>
2014-06-30 14:19:54 -0400

    added position attribute when using explore

--- app/controllers/api/v1/explore_controller.rb
+++ app/controllers/api/v1/explore_controller.rb
@@ -3,20 +3,25 @@ module Api
     class ExploreController < ApplicationController #Api::BaseController
 
       respond_to :json
-
+      
       def index
+        @positions = {}
+        position = 1
         @simple_events = SimpleEvent.sorted
-        puts DateTime.now.utc
         @explore = []
         for event in @simple_events
-          puts event
           if !event.start_date.past?
+            if event.image_url = "null"
+              event.image_url = "/images/event.png"
+            end
+            @positions[event.id] = position
             @explore << event
+            position += 1
           end
           break if @explore.count == 5
         end
-
       end
+
     end
   end
 end
--- app/controllers/api/v1/simple_events_controller.rb
+++ app/controllers/api/v1/simple_events_controller.rb
@@ -6,7 +6,6 @@ module Api
     # :except => [:index, :show]
 
     respond_to :json
-    # @default_image_url = "/images/event.png"
 
     def index
       @simple_events = SimpleEvent.sorted
@@ -48,6 +47,7 @@ module Api
         params.require(:simple_event).permit(:title, :event_description, :institution_id, :user_id, :department_id, :club_id, :circle_id, :event_url, :open, :image_url, :comment_count, :start_date, :end_date)
 
       end
+
     end
   end
 end
