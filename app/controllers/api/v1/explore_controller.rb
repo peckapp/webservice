7e11fffaea6c9d0a6ed983e185c79827fc0d52c1
Anthoney Tsou <anttsou@gmail.com>
2014-06-30 14:21:15 -0400

    Explore details

--- app/controllers/api/v1/explore_controller.rb
+++ app/controllers/api/v1/explore_controller.rb
@@ -5,13 +5,17 @@ module Api
       respond_to :json
 
       def index
+        @positions ={}
+        position = 1
         @simple_events = SimpleEvent.sorted
         puts DateTime.now.utc
         @explore = []
         for event in @simple_events
           puts event
           if !event.start_date.past?
+            @positions[event.id] = position
             @explore << event
+            position += 1
           end
           break if @explore.count == 5
         end
