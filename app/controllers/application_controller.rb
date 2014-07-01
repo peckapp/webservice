d5498d32c60f4327e849a6a8f258b291a4229892
Anthoney Tsou <anttsou@gmail.com>
2014-07-01 13:55:21 -0400

    Tried to make a template for api controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  def institution_index(model)
-    if params[:institution_id]
-      models = model.where(:institution_id => params[:institution_id])
+  def index(model, *parameter)
+    if params[parameter]
+      models = model.where(parameter => params[parameter])
     else
       models = model.all
     end
     return models
   end
 
-  def institution_show(model)
-    if params[:institution_id]
-      theModel = model.where(:institution_id => params[:institution_id]).find(params[:id])
+  def show(model, *parameter)
+    if params[parameter]
+      theModel = model.where(parameter => params[parameter).find(params[:id])
     else
       theModel = model.find(params[:id])
     end
