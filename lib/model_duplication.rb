a1f178cfb3a649d716575f794daa6b1ccd12b5ce
kujenga <ataylor0123@gmail.com>
2014-07-03 21:25:06 -0400

    attempts to get the lib module working, strange errors

--- lib/model_duplication.rb
+++ lib/model_duplication.rb
@@ -1,4 +1,12 @@
-class ModelDupliation
+# this class is intended to make it east to determine whether or not something exists in the database already
+# by either using a specified hash of the interested parameters and their values in the current object
+# that can be sent to the database as a query
+
+module ModelDupliation
+
+  def initialize
+
+  end
 
   def self.model_match_exists(object, hash)
     # method only applies to subclass models of the rails ActiveRecord::Base class
@@ -17,8 +25,9 @@ class ModelDupliation
 
     if ! self.model_match_exists(object,hash)
       object.save
+      return true
     else
-      # do nothing
+      return false
     end
 
   end
