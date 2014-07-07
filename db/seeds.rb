f6593ab41a8ba89570dd19a437e428ea332b4a4c
kujenga <ataylor0123@gmail.com>
2014-07-07 16:53:28 -0400

    changed background processing over to sidekiq with sidetiq for scheduling, added the web interface for sidekiq, and finished the model_duplication tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# basic internal institutions for the development database
+Institution.create( name: "Demo Institution", street_address: "1234 Main St.", city: "Williamstown", state: "MA", country: "USA", gps_longitude: -73.202887, gps_latitude: 42.710570, range: 10.0, configuration_id: 1, api_key: "DEMO01267" )
+Institution.create( name: "Development", street_address: "123 Main St.", city: "Anytown", state: "NY", country: "USA", gps_longitude: -74.062958, gps_latitude: 40.869911, range: 15.0, configuration_id: 1, api_key: "DEVELOPMENT" )
+
+# May also want to seed the configuration files for the database here
