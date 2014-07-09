dba085f696bdfc3ee89e721b50ee2623c25768fb
JDrobro <jsd1@williams.edu>
2014-07-09 11:25:32 -0400

    fixed dining and menu items controller + views

--- app/controllers/api/v1/dining_places_controller.rb
+++ app/controllers/api/v1/dining_places_controller.rb
@@ -8,33 +8,20 @@ module Api
       respond_to :json
 
       def index
-        if params[:dining_period_id]
-          @dining_places = DiningPeriod.find(params[:dining_period_id]).dining_places
-        elsif params[:menu_item_id]
-          @dining_places = MenuItem.find(params[:menu_item_id]).dining_places
-        else
-          @dining_places = specific_index(DiningPlace, params)
-        end
+        @dining_places = specific_index(DiningPlace, params)
       end
 
       def show
-        if params[:dining_period_id]
-          @dining_place = DiningPeriod.find(params[:dining_period_id]).dining_places.find(params[:id])
-        elsif params[:menu_item_id]
-          @dining_place = MenuItem.find(params[:menu_item_id]).dining_places.find(params[:id])
-        else
-          @dining_place = specific_show(DiningPlace, :institution_id)
-        end
+        @dining_place = specific_show(DiningPlace, params[:id])
       end
 
       def create
         @dining_place = DiningPlace.create(dining_place_create_params)
 
-        @dining_period_id = dining_place_create_params[:dining_period_id]
-          DiningPeriod.find(@dining_period_id).dining_places << @dining_place
-
-        @menu_item_id = dining_place_create_params[:menu_item_id]
-          MenuItem.find(@menu_item_id).dining_places << @dining_place
+        if dining_place_create_params[:dining_opportunity_id]
+          @dining_opportunity_id = dining_place_create_params[:dining_opportunity_id]
+          DiningOpportunity.find(@dining_opportunity_id).dining_places << @dining_place
+        end
       end
 
       def update
@@ -49,7 +36,7 @@ module Api
       private
 
         def dining_place_create_params
-          params.require(:dining_place).permit(:institution_id, :name, :details_link, :gps_longitude, :gps_latitude, :range, :dining_period_id, :menu_item_id)
+          params.require(:dining_place).permit(:institution_id, :name, :details_link, :gps_longitude, :gps_latitude, :range, :dining_opportunity_id)
         end
 
         def dining_place_update_params
--- app/views/api/v1/dining_opportunities/create.json.rabl
+++ app/views/api/v1/dining_opportunities/create.json.rabl
@@ -2,4 +2,4 @@ object :@dining_opportunity
 
 attributes :id, :dining_opportunity_type, :institution_id, :created_at, :updated_at
 
-node(:dining_period_id) {@dining_period_id}
+node(:dining_place_id) {@dining_place_id}
