18c94e82c870fff5f647dedac04602892d7178f8
kujenga <ataylor0123@gmail.com>
2014-07-08 22:25:02 -0400

    added basic test for current_or_create_new method

--- test/lib/model_duplication_test.rb
+++ test/lib/model_duplication_test.rb
@@ -32,10 +32,26 @@ class ModelDuplicationTest < ActionController::TestCase
     assert ModelDuplication.non_duplicative_save(event)
   end
 
+  test "current or create new for preexisting object"
+    menu_item = MenuItem.find(1)
+    result = ModelDuplication.current_or_create_new(menu_item)
+    assert_not result.blank?
+  end
+
+  test "current or create new for non-existant object"
+    menu_item = self.random_menu_item
+    result = ModelDuplication.current_or_create_new(menu_item)
+    assert_not result.blank?
+  end
+
   private
 
     def self.random_event
       return SimpleEvent.new(title: "brand new event #{rand(100)}", event_description: "#{rand(100)} is a random number", institution_id: 2, start_date: DateTime.current, end_date: DateTime.now + 1.hour)
+    end
+
+    def self.random_menu_item
+      return MenuItem.new(name: "menu item #{rand(100)}", category: "category #{rand(10)}", institution_id: rand(Institution.all.count), dining_opportunity_id: 1, dining_place_id: 1, date_available: DateTime.now)
     end
 
 end
