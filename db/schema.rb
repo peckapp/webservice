b59e76f6c1b8f476f330bccbc7b7d52f67d470a0
JDrobro <jsd1@williams.edu>
2014-07-15 11:46:57 -0400

    migration for user password hash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714162318) do
+ActiveRecord::Schema.define(version: 20140715154224) do
 
   create_table "activity_logs", force: true do |t|
     t.integer  "sender",           null: false
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 20140714162318) do
   add_index "athletic_teams", ["institution_id"], name: "index_athletic_teams_on_institution_id", using: :btree
   add_index "athletic_teams", ["sport_name"], name: "index_athletic_teams_on_sport_name", using: :btree
 
+  create_table "attendees_users", id: false, force: true do |t|
+    t.integer "event_attendee_id", null: false
+    t.integer "user_id",           null: false
+  end
+
+  add_index "attendees_users", ["event_attendee_id", "user_id"], name: "index_attendees_users_on_event_attendee_id_and_user_id", using: :btree
+
   create_table "circle_members", force: true do |t|
     t.integer  "circle_id",      null: false
     t.integer  "user_id",        null: false
@@ -81,6 +88,13 @@ ActiveRecord::Schema.define(version: 20140714162318) do
   add_index "circle_members", ["circle_id"], name: "index_circle_members_on_circle_id", using: :btree
   add_index "circle_members", ["invited_by"], name: "index_circle_members_on_invited_by", using: :btree
   add_index "circle_members", ["user_id"], name: "index_circle_members_on_user_id", using: :btree
+
+  create_table "circle_members_users", id: false, force: true do |t|
+    t.integer "user_id",          null: false
+    t.integer "circle_member_id", null: false
+  end
+
+  add_index "circle_members_users", ["user_id", "circle_member_id"], name: "circle_members_users_index", using: :btree
 
   create_table "circles", force: true do |t|
     t.integer  "institution_id", null: false
@@ -236,6 +250,13 @@ ActiveRecord::Schema.define(version: 20140714162318) do
   add_index "institutions", ["configuration_id"], name: "index_institutions_on_configuration_id", using: :btree
   add_index "institutions", ["name"], name: "index_institutions_on_name", using: :btree
 
+  create_table "inviters_users", id: false, force: true do |t|
+    t.integer "event_attendee_id", null: false
+    t.integer "user_id",           null: false
+  end
+
+  add_index "inviters_users", ["event_attendee_id", "user_id"], name: "index_inviters_users_on_event_attendee_id_and_user_id", using: :btree
+
   create_table "locations", force: true do |t|
     t.integer  "institution_id", null: false
     t.string   "name",           null: false
@@ -373,12 +394,13 @@ ActiveRecord::Schema.define(version: 20140714162318) do
     t.text     "blurb"
     t.string   "facebook_link"
     t.string   "facebook_token"
-    t.string   "password_digest"
+    t.string   "password_hash"
     t.string   "api_key"
     t.boolean  "active",               default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "password_salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
