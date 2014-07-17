6fab1f1a592fdb699fd2f91980857f06ea986806
kujenga <ataylor0123@gmail.com>
2014-07-16 23:17:07 -0400

    added ActiveAdmin generated install files

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
