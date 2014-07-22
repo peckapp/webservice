1b1a3bcae37f1c1d91058093b8232f45633313b1
kujenga <ataylor0123@gmail.com>
2014-07-22 00:37:50 -0400

    attempted fixes for broken admin setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  # Active Admin route configuration
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -92,6 +94,7 @@ Rails.application.routes.draw do
       resources :simple_events
     end
   end
+
 
   # api status and version information
   get 'api', to: 'api#index', via: [:get]
