524b9224751503bc743c61429f86aaf4b23cf906
Anthoney Tsou <anttsou@gmail.com>
2014-07-23 09:25:36 -0400

    increased max file size to 5 mb

--- app/models/simple_event.rb
+++ app/models/simple_event.rb
@@ -25,7 +25,7 @@ class SimpleEvent < ActiveRecord::Base
   ### Event Photo Attachments ###
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 2.megabytes
+  validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 5.megabytes
 
   ###############################
   ##                           ##
