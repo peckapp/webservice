415372b5ffd6a82da81384a34103f340628c8d33
JDrobro <jsd1@williams.edu>
2014-07-23 17:53:13 -0400

    possible to create many subscriptions within the same request (still needs work on view depending on what app needs)

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,7 +7,7 @@ module Api
       respond_to :json
 
       def index
-          @subscriptions = specific_index(Subscription, params)
+        @subscriptions = specific_index(Subscription, params)
       end
 
       def show
@@ -15,12 +15,17 @@ module Api
       end
 
       def create
-        @subscription = Subscription.create(subscription_params)
+        @subscriptions = []
+
+        params[:subscriptions].each do |parameters|
+          this_subscription = Subscription.create(subscription_create_params(parameters))
+          @subscriptions << this_subscription
+        end
       end
 
       def update
         @subscription = Subscription.find(params[:id])
-        @subscription.update_attributes(subscription_params)
+        @subscription.update_attributes(subscription_update_params)
       end
 
       def destroy
@@ -29,11 +34,14 @@ module Api
 
       private
 
-        def subscription_params
-
+        def subscription_update_params
           params.require(:subscription).permit(:institution_id, :user_id, :category, :subscribed_to)
-
         end
+
+        def subscription_create_params(parameters)
+          parameters.permit(:institution_id, :user_id, :category, :subscribed_to)
+        end
+
     end
   end
 end
