b226e91ba20c00bc163d4467ae0953b68a424689
kujenga <ataylor0123@gmail.com>
2014-07-30 14:45:41 -0400

    added proper env variable loading to initializers for push certificate passwords

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,18 +1,26 @@
 # loads in environment variables for proper environment if a file exists locally
 
 module EnvironmentVariables
+  # extends rails application class
   class Application < Rails::Application
     config.before_configuration do
-      env_file = Rails.root.join("config", 'environment_variables.yml').to_s
-
-      if File.exists?(env_file)
+      env_file = Rails.root.join('config', 'environment_variables.yml').to_s
+      if File.exist?(env_file)
         yaml_for_env = YAML.load_file(env_file)[Rails.env]
-        if ! yaml_for_env.blank? # protects against empty yaml entries
+        unless yaml_for_env.blank? # protects against empty yaml entries
           yaml_for_env.each do |key, value|
             ENV[key.to_s] = value
           end # end YAML.load_file
         end # end ! yaml_for_env.blank?
       end # end if File.exists?
     end # end config.before_configuration
+
+    # properly assign environment variables loaded here
+    config.after_initialize do
+      # Apple Push Notification Service certificate key
+      APNS.pass = ENV['PUSH_CERT_PASS']
+      # Google Cloud Messenger API key
+      GCM.key = ENV['GCM_API_KEY']
+    end # end config.after_initialize
   end # end class
 end # end module
