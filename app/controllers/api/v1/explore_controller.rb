ee9acd18e524ecf157d4090acba5a6b12c920b20
Anthoney Tsou <anttsou@gmail.com>
2014-07-30 14:47:23 -0400

    explore supports bot h announcements and events

--- app/controllers/api/v1/explore_controller.rb
+++ app/controllers/api/v1/explore_controller.rb
@@ -5,23 +5,71 @@ module Api
       respond_to :json
 
       def index
-        @positions = {}
-        position = 1
+
+        #### Simple Events ####
+        @event_positions = {}
+
+        event_position = 1
+
         @simple_events = specific_index(SimpleEvent, params).sorted
-        @explore = []
+
+        @explore_events = []
+
         for event in @simple_events
           if !event.start_date.past?
             # if event.image_url = "null"
             #   event.image_url = "/images/event.png"
             # end
-            @positions[event.id] = position
-            @explore << event
-            position += 1
+            @event_positions[event.id] = event_position
+            @explore_events << event
+            event_position += 1
           end
-          break if @explore.count == 5
+          break if @explore_events.count == 5
+        end
+
+        # initialize hash mapping events to arrays of likers
+        @likes_for_explore_events = {}
+
+        @explore_events.each do |event|
+          likers = []
+          event.likers(User).each do |user|
+            likers << user.id
+          end
+
+          @likes_for_explore_events[event] = likers
+        end
+
+        #### Announcements ####
+        @announcement_positions = {}
+
+        announcement_position = 1
+
+        # announcements sorted on creation date
+        @announcements = specific_index(Announcement, params).sorted
+
+        @explore_announcements = []
+
+        for announcement in @announcements
+          @announcement_positions[announcement.id] = announcement_position
+          @explore_announcements << announcement
+          announcement_position += 1
+          break if @explore_announcements.count == 5
+        end
+
+        @likes_for_explore_announcements = {}
+
+        # for each announcement
+        @explore_announcements.each do |announcement|
+          likers = []
+
+          # for each liker in the array of announcement likers, add the user's id
+          announcement.likers(User).each do |user|
+            likers << user.id
+          end
+
+          @likes_for_explore_announcements[announcement] = likers
         end
       end
-
     end
   end
 end
--- app/views/api/v1/explore/index.json.rabl
+++ app/views/api/v1/explore/index.json.rabl
@@ -1,7 +1,24 @@
-collection :@explore
+child :@explore_events do
 
-attributes :id, :title, :event_description, :institution_id, :user_id, :department_id, :club_id, :circle_id, :event_url, :public, :comment_count, :start_date, :end_date, :created_at, :updated_at
+  attributes :id, :title, :event_description, :institution_id, :user_id, :department_id, :club_id, :circle_id, :event_url, :public, :comment_count, :start_date, :end_date, :created_at, :updated_at
 
-node :position do |pos|
-   @positions[pos.id]
+  node :position do |event|
+     @event_positions[event.id]
+  end
+
+  node :likes do |explore_event|
+    @likes_for_explore_events[explore_event]
+  end
+end
+
+child :@explore_announcements do
+  attributes :id, :title, :announcement_description, :institution_id, :user_id, :department_id, :club_id, :circle_id, :public, :comment_count, :created_at, :updated_at
+
+  node :position do |announcement|
+     @announcement_positions[announcement.id]
+  end
+
+  node :likes do |explore_announcement|
+    @likes_for_explore_announcements[explore_announcement]
+  end
 end
