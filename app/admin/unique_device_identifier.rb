acc3ab246e08e0932bba8e5dabb0efec8ca75d42
kujenga <ataylor0123@gmail.com>
2014-07-31 13:47:04 -0400

    added unique identifiers to active admin

--- app/admin/unique_device_identifier.rb
+++ app/admin/unique_device_identifier.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register UniqueDeviceIdentifiers do
+ActiveAdmin.register UniqueDeviceIdentifier do
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
