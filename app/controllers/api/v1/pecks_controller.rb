c8400239ab7b31fff94f97f50e77a012e479d1a8
Anthoney Tsou <anttsou@gmail.com>
2014-08-01 10:37:32 -0400

    put pecks into pecks controller

--- app/controllers/api/v1/pecks_controller.rb
+++ app/controllers/api/v1/pecks_controller.rb
@@ -8,7 +8,7 @@ module Api
       respond_to :json
 
       def index
-          @pecks = specific_index(Peck, params)
+        @pecks = specific_index(Peck, params)
       end
 
       def show
@@ -16,12 +16,17 @@ module Api
       end
 
       def create
-        @peck = Peck.create(peck_params)
+        peck_create_params = params[:peck]
+        token = peck_create_params.delete(:token)
+        @peck = Peck.create(peck_params(peck_create_params))
+        if @peck.send_push_notification
+          APNS.send_notification(token, alert: @peck.message, badge: 1, sound: 'default')
+        end
       end
 
       def update
         @peck = Peck.find(params[:id])
-        @peck.update_attributes(peck_params)
+        @peck.update_attributes(peck_update_params)
       end
 
       def destroy
@@ -29,7 +34,11 @@ module Api
       end
 
       private
-        def peck_params
+        def peck_params(parameters)
+          parameters.permit(:user_id, :institution_id, :notification_type, :message, :send_push_notification, :invited_by)
+        end
+
+        def peck_update_params
           params.require(:peck).permit(:user_id, :institution_id, :notification_type, :message, :send_push_notification, :invited_by)
         end
     end
