9665972242e3ceedadeba4d3839b673e86341f3c
Anthoney Tsou <anttsou@gmail.com>
2014-08-04 10:18:09 -0400

    creates the creator explicitly

--- app/controllers/api/v1/circles_controller.rb
+++ app/controllers/api/v1/circles_controller.rb
@@ -40,6 +40,9 @@ module Api
         @member_ids = []
 
         if @circle
+          @creator = CircleMember.create(accepted: true, user_id: @circle.user_id, circle_id: @circle.id, invited_by: @circle.user_id)
+          
+          @circle.circle_members << @creator
           # takes the array of circle members found in the circle block
           members = the_circle_members
           # members should be an array of integers corresponding to user ids
@@ -49,9 +52,7 @@ module Api
 
             the_user = User.find(mem_id)
             # the creator must always have an accepted tag of true.
-            if the_user.id == @circle.user_id
-              member.update_attributes(accepted: true)
-            end
+
 
             # adds the member to the array of circle members for the created circle
             @circle.circle_members << member
@@ -59,7 +60,6 @@ module Api
             ### Push Notification Stuff ###
 
             # As long as the user is not the creator.
-            if mem_id != @circle.user_id
               # create a peck for the user in the passed array
               Peck.create(user_id: mem_id, institution_id: @circle.institution_id, notification_type: "circle_invite", message: the_message, invited_by: @circle.user_id, invitation: member.id)
               the_user.unique_device_identifiers.each do |device|
@@ -74,7 +74,6 @@ module Api
                 end
               end
             end
-          end
 
           # circle members
           circle_mems = @circle.circle_members
