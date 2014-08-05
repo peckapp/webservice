f3651eec48a7ddcf381e48b1d5e03fffbe7dbfd2
kujenga <ataylor0123@gmail.com>
2014-08-05 12:39:45 -0400

    added content frame to scraping

--- app/admin/scraping.rb
+++ app/admin/scraping.rb
@@ -2,6 +2,8 @@ ActiveAdmin.register_page 'Scraping' do
 
   content do
     para 'Main Scraping page, will have lots of cool tools!'
+    div '<object type="text/html" data="http://validator.w3.org/" width="800px" height="600px" style="overflow:auto;border:5px ridge blue"></object>'.html_safe
+    # script "$(\"#siteloader\") .html('<object data=\"http://www.peckapp.com\"/>');".html_safe
   end
 
 end
