ea05a8087d18aedb0ffe14507c14a55406b46b70
kujenga <ataylor0123@gmail.com>
2014-08-08 12:00:17 -0400

    williams dining updates

--- app/workers/specific_scrape/williams_dining.rb
+++ app/workers/specific_scrape/williams_dining.rb
@@ -12,8 +12,43 @@ module SpecificScrape
     WILLIAMS_MENUS = 'http://nutrition.williams.edu/NetNutrition/Home.aspx'
 
     def perform
+      williams = Institution.where(api_key: 'WILLIAMS').first
+      resource_type = ResourceType.where(resource_name: 'menu_item').first
+      if williams.blank? || resource_type.blank?
+        logger.error 'Either \'WILLIAMS\' institution or \'menu_item\' resource not found for dining scrape'
+        return
+      end
+      resources = ScrapeResource.where(resource_type_id: resource_type.id, validated: true, institution_id: williams.id)
+      logger.info "found #{resources.count} resources"
+      if resources.blank?
+        logger.warn 'No scrape resources for middlebury dining menus'
+        resources << ScrapeResource.current_or_create_new(url: WILLIAMS_MENUS, institution_id: williams.id,
+                                                          resource_type_id: resource_type.id)
+      end
 
+      logger.info 'creating headless watir browser'
+      headless = Headless.new
+      headless.start
+      b = Watir::Browser.new
+
+      begin
+        resources.each do |r|
+          logger.info "scraping resource #{r.id}: #{r.kind}, #{r.info}"
+          b.goto r.url
+          logger.info 'entering williams dining scraping loop'
+          scrape_places(b, williams.id, r.id)
+        end
+      rescue => error
+        raise error
+      ensure
+        b.close
+        headless.destroy
+      end
     end
 
+    #
+    def scrape_places(d, inst_id, sr_id)
+
+    end
   end
 end
