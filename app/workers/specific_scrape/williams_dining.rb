e18c52a8472c8cb29511a8d2320faa51d4414b80
kujenga <ataylor0123@gmail.com>
2014-08-11 14:03:36 -0400

    working menu iteration for williams dining using watir

--- app/workers/specific_scrape/williams_dining.rb
+++ app/workers/specific_scrape/williams_dining.rb
@@ -75,9 +75,12 @@ module SpecificScrape
             dining_place = DiningPlace.current_or_create_new(name: ts.selector, institution_id: inst_id)
 
             # for williams, at every specific place there is a table of days with meals inside of them
-            logger.info 'clicking child'
+            logger.info "clicking child: #{child.selector}"
             b.link(text: child.selector).click
+            be_nice
             scrape_dates(b, dining_place, inst_id, sr_id)
+            logger.info 'done scraping dates'
+            be_nice
             go_back(b)
           end
         end # end child iteration
@@ -87,39 +90,49 @@ module SpecificScrape
 
     # scrapes dates using built-in css selectors since these are the same for each page
     def scrape_dates(b, place_id, inst_id, sr_id)
-      logger.info 'scraping dates'
+      logger.info "scraping dates for #{b.tds(css: 'tr .cbo_nn_menuCell').count} cells"
       # iterates over the cells for each day
-      b.trs(css: '.cbo_nn_menuCell').each do |cell|
+      cell_max = b.tds(css: 'tr .cbo_nn_menuCell').count
+      (1..cell_max).each do |i|
+        cell = b.tds(css: 'tr .cbo_nn_menuCell')[i - 1]
+        logger.info 'starting date iteration'
         date = cell.td.text
-        logger.info "iterating over date #{date}"
-        # should only be one
-        cell.tds(css: '.cbo_nn_menuLink').each do |opp_link|
+        logger.info "iterating over date #{date} with #{cell.tds(css: '.cbo_nn_menuLink').count} links"
+
+        link_max = cell.tds(css: '.cbo_nn_menuLink').count
+        (1..link_max).each do |j|
+          # must be refreshed after any page navigation
+          cell = b.tds(css: 'tr .cbo_nn_menuCell')[i - 1]
+          opp_link = cell.tds(css: '.cbo_nn_menuLink')[j - 1]
+
+          logger.info "opp_link: #{opp_link}"
           opp_type = opp_link.text.downcase.camelize # creates a properly cased version of the dining opportunity
-          opp = DiningOpportunity.current_or_create_new(institution_id: inst_id, dining_opportunity_type: opp_type)
+          # opp = DiningOpportunity.current_or_create_new(institution_id: inst_id, dining_opportunity_type: opp_type)
 
           logger.info "clicking on link type: #{opp_type}"
           opp_link.click
           be_nice
-          panel_html = b.table(css: '.cbo_nn_itemGridTable').html
-          scrape_items_from_opportunity(panel_html, opp_id, date, place_id, inst_id, sr_id)
+          scrape_items_from_opportunity(panel_html, opp.id, date, place_id, inst_id, sr_id)
+          be_nice
+          go_back(b)
+          logger.info 'iterating over dining opportunities'
         end
       end
     end
 
     # passed in the html for the panel to be parsed as well as the necessary parameters for menu item creation
-    def scrape_items_from_opportunity(panel_html, opp_id, date, place_id, inst_id, sr_id)
-      html = Nokigiri::HTML(panel_html)
-
+    def scrape_items_from_opportunity(b, opp_id, date, place_id, inst_id, sr_id)
+      html = Nokogiri::HTML(b.table(css: '.cbo_nn_itemGridTable').html)
+      logger.info "scraping items from opportunity with id: #{opp_id}"
       category_name = nil
       html.css('tr').each do |row|
         if row.css('.cbo_nn_itemGroupRow').count > 0 # update category during iteration
           category_name = row.text
         elsif row.css('.cbo_nn_itemHover').count > 0 # create menu item with current category
-          mi = MenuItem.new(institution_id: inst_id, name: row.text, opportunity_id: opp_id,
-                            date: date, category: category_name)
+          mi = MenuItem.new(institution_id: inst_id, name: row.text, dining_opportunity_id: opp_id,
+                            dining_place_id: place_id, date_available: date, category: category_name)
           logger.info "created valid menu item? #{mi.valid?}"
         end
-
       end
     end
 
@@ -131,6 +144,7 @@ module SpecificScrape
     end
 
     def be_nice
+      logger.info 'sleeping...'
       # sleeps so as to avoid blasting requests repeatedly in a row
       sleep 1.0 + rand
     end
