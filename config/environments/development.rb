9d2d4f8bfbeb98f27d1e8dbdb871dc6a606f1544
Anthoney Tsou <anttsou@gmail.com>
2014-08-11 11:41:48 -0400

    mailer updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'peckboblee',
+    password: 'bobbylee8',
+    authentication: 'plain',
+    #enable_starttls_auto: true
+    }
   # devise
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # config.action_mailer.delivery_method = :smtp
