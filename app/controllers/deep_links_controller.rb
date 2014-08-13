d35c7bd5e9117d539bdf6d00bd9d136fd00a3d21
Anthoney Tsou <anttsou@gmail.com>
2014-08-13 18:13:06 -0400

    checks if the app is downloaded. if not, should go to app store

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -1,22 +1,17 @@
 class DeepLinksController < ApplicationController
-  before_action :confirm_minimal_access, except: [:native_peck, :set_request_format, :set_format_fallbacks]
-  before_action :set_request_format, :set_format_fallbacks
+  before_action :confirm_minimal_access, except: :native_peck
 
   def native_peck
     if mobile_request?
-      redirect_to("peckapp://")
+      begin
+        response = HTTParty.get("peckapp://", limit: 50)
+      rescue URI::InvalidURIError => encoding
+        redirect_to("https://itunes.apple.com/us/app/peck-williams/id702672553?mt=8")
+      else
+        redirect_to("peckapp://")
+      end
     else
       redirect_to("http://peckapp.com")
-    end
-  end
-
-  def set_request_format
-    request.format = :mobile if mobile_request?
-  end
-
-  def set_format_fallbacks
-    if request.format == :mobile
-      self.formats = [:mobile, :html]
     end
   end
 end
