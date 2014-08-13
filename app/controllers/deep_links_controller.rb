10b310bc5d2fec578de95420e199799650158ae8
Anthoney Tsou <anttsou@gmail.com>
2014-08-13 17:17:17 -0400

    hopefully renders the view if redirect does not happen

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -1,11 +1,22 @@
 class DeepLinksController < ApplicationController
-  before_action :confirm_minimal_access, except: [:native_peck]
+  before_action :confirm_minimal_access, except: [:native_peck, :set_request_format, :set_format_fallbacks]
+  before_action :set_request_format, :set_format_fallbacks
 
   def native_peck
     if mobile_request?
       redirect_to("peckapp://")
     else
       redirect_to("http://peckapp.com")
+    end
+  end
+
+  def set_request_format
+    request.format = :mobile if mobile_request?
+  end
+
+  def set_format_fallbacks
+    if request.format == :mobile
+      self.formats = [:mobile, :html]
     end
   end
 end
