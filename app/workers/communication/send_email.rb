50ce36217640dc3a4f2492f67763dce5e4826abb
Anthoney Tsou <anttsou@gmail.com>
2014-08-12 21:42:37 -0400

    testing push notifications for android

--- app/workers/communication/send_email.rb
+++ app/workers/communication/send_email.rb
@@ -1,10 +1,11 @@
 module Communication
   class SendEmail
     include Sidekiq::Worker
+    sidekiq_options :retry => false
 
     def perform(id)
       user = User.find(id)
-      UserMailer.registration_confirmation(user, id).deliver
+      UserMailer.registration_confirmation(user, id).deliver      
     end
   end
 end
