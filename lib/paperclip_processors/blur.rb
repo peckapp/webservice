517ba41cf1c4b964f95a7ce12e9eca244127100b
kujenga <ataylor0123@gmail.com>
2014-08-12 22:16:57 -0400

    paperclip now has gravity set properly

--- lib/paperclip_processors/blur.rb
+++ lib/paperclip_processors/blur.rb
@@ -6,6 +6,7 @@ module Paperclip
 
     # some default values, may be overridden in the models that use this processor
     DEFAULT_SIZE = '640X256'
+    DEFAULT_GRAVITY = 'center'
     OFFSET = '+0+0'
     RADIUS_SIGMA = '9x5'
     TINT_STRENGTH = 40 # range 0-100
@@ -22,6 +23,7 @@ module Paperclip
       @format              = options[:format]
 
       @size                = options[:size] || DEFAULT_SIZE
+      @gravity             = options[:gravity] || DEFAULT_GRAVITY
       @offset              = options[:offset] || OFFSET
       @radius_sigma        = options[:radius_sigma] || RADIUS_SIGMA
       @tint                = options[:tint] || TINT_STRENGTH
@@ -73,6 +75,7 @@ module Paperclip
       # -resize 640x256^ -gravity Center -crop 640x256+0+0 +repage -gaussian-blur 10X6 -normalize -fill black -colorize 40% -modulate 100,110
       trans = []
       trans << '-resize' << %("#{DEFAULT_SIZE}^")
+      trans << '-gravity' << %(#{DEFAULT_GRAVITY})
       trans << '-crop' << %("#{DEFAULT_SIZE}#{OFFSET}") << '+repage'
       trans << '+repage'
       trans << '-gaussian-blur' << %("#{RADIUS_SIGMA}")
