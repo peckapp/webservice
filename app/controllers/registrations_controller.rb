67b4cd741ff2390e030c5b92b46b0e5e5c8d8588
Anthoney Tsou <anttsou@gmail.com>
2014-08-12 20:04:52 -0400

    made email confirmation redirect to a more readable page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,8 +3,8 @@ class RegistrationsController < ApplicationController
 
   # sets the user's activity to true to make sure they're done registering.
   def confirm_email
-    user = User.find(params[:id])
-    user.update_attributes(active: true)
-    redirect_to("http://peckapp.com")
+    @user = User.find(params[:id])
+    @user.update_attributes(active: true)
+    # redirect_to("http://peckapp.com")
   end
 end
