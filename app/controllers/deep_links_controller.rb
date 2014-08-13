2f8687583f80427c807b818d27669bee4957dc5f
Anthoney Tsou <anttsou@gmail.com>
2014-08-13 19:40:17 -0400

    3rd attempt

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -3,16 +3,27 @@ class DeepLinksController < ApplicationController
 
   def native_peck
     if mobile_request?
-      uri = URI.parse("peckapp://")
       begin
-        response = HTTParty.get(uri, limit: 50)
+        the_uri = unshorten("peckapp://")
+        response = HTTParty.get(the_uri, limit: 50)
       rescue URI::InvalidURIError => encoding
         redirect_to("https://itunes.apple.com/us/app/peck-williams/id702672553?mt=8")
       else
         redirect_to("peckapp://")
       end
     else
-      redirect_to("http://peckapp.com")
+      redirect_to("peckapp://")
+    end
+
+    def unshorten(uri)
+      begin
+        response = HTTParty.get(uri, limit: 50)
+      rescue URI::InvalidURIError => error
+        bad_uri = error.message.match(/^bad\sURI\(is\snot\sURI\?\)\:\s(.*)$/)[1]
+        good_uri = URI.encode bad_uri
+        response = self.unshorten good_uri
+      end
+      response
     end
   end
 end
