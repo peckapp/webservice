636831f40536e4b0ecec33d7a32db12c1d9fe1c4
JDrobro <jsd1@williams.edu>
2014-08-14 13:54:57 -0400

    running tests on server because localhost is working perfectly (puts statement in explore controller) --> problem should now be fixed

--- lib/personalizer.rb
+++ lib/personalizer.rb
@@ -46,8 +46,10 @@ class Personalizer
       ## Scoring boost for circle friends
       friend_boost = 0
       top_circle_friends.each do |friend|
-        if attendees && attendees.include? friend[0]
 
+        logger.info("----> #{attendees} <----")
+        if attendees != nil && attendees.include?(friend[0])
+          logger.info("----> HERE <----")
           if weights.circle_friend_boost(friend[1], circle_count) > MAX_FRIEND_SCORE
             friend_score = MAX_FRIEND_SCORE
           else
