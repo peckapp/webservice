7548f6f2684f0ce85cadc06da9dc83e8ab139b58
JDrobro <jsd1@williams.edu>
2014-08-14 15:25:16 -0400

    more tests

--- app/workers/explore/builder.rb
+++ app/workers/explore/builder.rb
@@ -29,7 +29,7 @@ module Explore
       analysis_group_epoch = SimpleEvent.where(start_date: Time.now.to_i..1.month.from_now.to_i).pluck(:id)
       analysis_group_ids = (analysis_group_datetime + analysis_group_epoch).uniq
 
-      Rails.logger.info "------> #{Time.now.to_i} -------- #{1.month.from_now.to_i} <-------"
+      logger.info "------> #{Time.now.to_i} -------- #{1.month.from_now.to_i} <-------"
 
       analysis_group = SimpleEvent.where(id: analysis_group_ids)
 
