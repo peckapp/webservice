aa8182af6fe53d4e38ec8739d610b9228fd296a0
Anthoney Tsou <anttsou@gmail.com>
2014-08-15 14:29:43 -0400

    clicking on the event should now lead to the actual event

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -3,17 +3,19 @@ class DeepLinksController < ApplicationController
 
   def native_peck
     if apple_request?
-      redirect_to apple_deep_links_url
+      redirect_to apple_deep_links_url(event: params[:event_id])
     elsif android_request?
-      redirec_to android_deep_links_url
+      redirect_to android_deep_links_url(event: params[:event_id] )
     else
-      @simple_event = SimpleEvent.find(61)
+      @simple_event = SimpleEvent.find(params[:event_id])
     end
   end
 
   def apple
+    @event_id = params[:event]
   end
 
   def android
+    @event_id = params[:event]
   end
 end
