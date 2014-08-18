dd013f8a84bd19d7c647f903df4f860ac8a70328
Anthoney Tsou <anttsou@gmail.com>
2014-08-18 14:52:57 -0400

    reverted back to setting passwords the original way

--- app/controllers/mobile_resets_controller.rb
+++ app/controllers/mobile_resets_controller.rb
@@ -1,17 +1,17 @@
 class MobileResetsController < ApplicationController
   before_action :confirm_minimal_access, except: [:apple, :android, :desktop, :confirmation]
-  before_action :the_new_pass_params
 
   def desktop
     the_id = params.delete(:id)
     user = User.find(the_id)
     new_pass_params = pass_reset_params
     user.update_attributes(new_pass_params)
+    temp_pass = new_pass_params[:password]
 
     if apple_request?
-      redirect_to apple_mobile_resets_url(id: user.id)
+      redirect_to apple_mobile_resets_url(id: user.id, temp: temp_pass)
     elsif android_request?
-      redirect_to android_mobile_resets_url(id: user.id)
+      redirect_to android_mobile_resets_url(id: user.id, temp: temp_pass)
     else
       redirect_to confirmation_mobile_resets_url
     end
@@ -19,22 +19,18 @@ class MobileResetsController < ApplicationController
 
   def apple
     @user = User.find(params[:id])
-    @temp_pass = @the_temp_password
+    @temp_pass = params[:temp]
   end
 
   def android
     @user = User.find(params[:id])
-    @temp_pass = @the_temp_password
+    @temp_pass = params[:temp]
   end
 
   def confirmation
   end
 
   private
-
-  def the_new_pass_params
-    @the_temp_password = params[:user][:password]
-  end
 
   def pass_reset_params
     params.require(:user).permit(:password)
