fdc0e61aaf899cf9a7131e7aad687511a89b76b7
Anthoney Tsou <anttsou@gmail.com>
2014-08-18 15:55:52 -0400

    removed event id because of fb encryption; also removed the event view from nonmobile since getting event id may not be possible

--- app/controllers/deep_links_controller.rb
+++ app/controllers/deep_links_controller.rb
@@ -3,19 +3,15 @@ class DeepLinksController < ApplicationController
 
   def native_peck
     if apple_request?
-      redirect_to apple_deep_links_url(event: params[:event_id])
+      redirect_to apple_deep_links_url
     elsif android_request?
-      redirect_to android_deep_links_url(event: params[:event_id] )
-    else
-      @simple_event = SimpleEvent.find(params[:event_id])
+      redirect_to android_deep_links_url
     end
   end
 
   def apple
-    @event_id = params[:event]
   end
 
   def android
-    @event_id = params[:event]
   end
 end
