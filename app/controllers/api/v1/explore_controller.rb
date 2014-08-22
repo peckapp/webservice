92ad3ebedfedad4cbfe453a145fb9543b183ffcd
JDrobro <jsd1@williams.edu>
2014-08-21 20:43:28 -0400

    personalizer for athletic events implemented

--- app/controllers/api/v1/explore_controller.rb
+++ app/controllers/api/v1/explore_controller.rb
@@ -15,8 +15,9 @@ module Api
         dc = PeckDalli.client
         simple_scores = dc.get("campus_simple_explore_#{auth_inst_id}")
         announcement_scores = dc.get("campus_announcement_explore_#{auth_inst_id}")
+        athletic_scores = dc.get("campus_athletic_explore_#{auth_inst_id}")
 
-        if simple_scores.blank? || announcement_scores.blank?
+        if simple_scores.blank? || announcement_scores.blank? || athletic_scores.blank?
           # trigger campus explore calculation, or perform manually.
           Explore::Builder.perform_async(auth_inst_id)
 
@@ -32,6 +33,7 @@ module Api
           personalizer = Personalizer.new
           personal_simple_scores = personalizer.perform_events(simple_scores, auth_user_id, auth_inst_id)
           personal_announcement_scores =  personalizer.perform_announcements(announcement_scores, auth_user_id, auth_inst_id)
+          personal_athletic_scores = personalizer.perform_events(athletic_scores, auth_user_id, auth_inst_id)
 
           ### Scale announcement scores to z-score ###
           ## se = simple events
@@ -47,6 +49,7 @@ module Api
           ann_mean = ann_score_stats.mean
           ann_std_dev = ann_score_stats.standard_deviation
 
+          # scaling every announcement score to the simple event scores
           personal_announcement_scores.each do |ann|
             announcement_z = (ann[1] - ann_mean) / ann_std_dev
             ann[1] = se_mean + (announcement_z * se_std_dev)
@@ -56,20 +59,27 @@ module Api
           explore_ids = []
           @simple_explore_scores = {}
           @announcement_explore_scores = {}
+          @athletic_explore_scores = {}
+
+          # enumerators to iterate over each array AS NEEDED
           ann_enumerator = personal_announcement_scores.to_enum
           se_enumerator = personal_simple_scores.to_enum
+          ath_enumerator = personal_athletic_scores.to_enum
 
           # top scores of each explore array
           se_score = se_enumerator.next
           ann_score = ann_enumerator.next
+          ath_score = ath_enumerator.next
 
           # booleans to prevent StopIteration exception
           some_simple_events_left = true
           some_announcements_left = true
+          some_athletic_events_left = true
 
           # check next element of each array and take the higher score
           while explore_ids.size < NUMBER_OF_EXPLORE_ITEMS
-            if some_simple_events_left && se_score[1] > ann_score[1]
+            if some_simple_events_left && se_score[1] > ann_score[1] && se_score[1] > ath_score[1]
+              # check if event was organized by current user
               unless user_events.include?(se_score[0])
                 explore_ids << ['SimpleEvent', se_score[0]]
                 @simple_explore_scores[se_score[0]] = se_score[1]
@@ -81,42 +91,61 @@ module Api
               rescue StopIteration
                 some_simple_events_left = false
               end
-            elsif some_announcements_left
+            elsif some_announcements_left && ann_score[1] > se_score[1] && ann_score[1] > ath_score[1]
+              # check if announcement was posted by current user
               unless user_announcements.include?(ann_score[0])
                 explore_ids << ['Announcement', ann_score[0]]
                 @announcement_explore_scores[ann_score[0]] = ann_score[1]
               end
+
+              # make sure enumerator has a next element
               begin
                 ann_score = ann_enumerator.next
               rescue StopIteration
                 some_announcements_left = false
               end
+            elsif some_athletic_events_left
+              explore_ids << ['AthleticEvent', ath_score[0]]
+              @athletic_explore_scores[ath_score[0]] = ath_score[1]
+
+              # make sure enumerator has a next element
+              begin
+                ath_score = ath_enumerator.next
+              rescue StopIteration
+                some_athletic_events_left = false
+              end
             end
 
-            break unless some_simple_events_left || some_announcements_left
+            break unless some_simple_events_left || some_announcements_left || some_athletic_events_left
           end
 
           # split up announcement / simple event ids for db query
           announcement_ids = []
           simple_event_ids = []
+          athletic_event_ids = []
           explore_ids.each do |id|
             if id[0] == 'SimpleEvent'
               simple_event_ids << id[1]
             elsif id[0] == 'Announcement'
               announcement_ids << id[1]
+            elsif id[0] == 'AthleticEvent'
+              athletic_event_ids << id[1]
             end
           end
 
           # query db for the correct explore items
           @explore_events = SimpleEvent.where(id: simple_event_ids).where.not(user_id: auth_user_id)
           @explore_announcements = Announcement.where(id: announcement_ids).where.not(user_id: auth_user_id)
+          @explore_athletics = AthleticEvent.where(id: athletic_event_ids)
 
           # initialize hash mapping events to arrays of likers
           @likes_for_explore_events = {}
           @likes_for_explore_announcements = {}
+          @likes_for_explore_athletics = {}
 
           all_simple_likes = Like.where(likeable_type: 'SimpleEvent', likeable_id: simple_event_ids).pluck(:likeable_id, :liker_id)
           all_announcement_likes = Like.where(likeable_type: 'Announcement', likeable_id: announcement_ids).pluck(:likeable_id, :liker_id)
+          all_athletic_likes = Like.where(likeable_type: 'AthleticEvent', likeable_id: athletic_event_ids).pluck(:likeable_id, :liker_id)
 
           all_simple_likes.each do |like|
             if @likes_for_explore_events[like[0]]
@@ -131,6 +160,14 @@ module Api
               @likes_for_explore_announcements[like[0]] << like[1]
             else
               @likes_for_explore_announcements[like[0]] = [like[1]]
+            end
+          end # end announcement likes iteration
+
+          all_athletic_likes.each do |like|
+            if @likes_for_explore_athletics[like[0]]
+              @likes_for_explore_athletics[like[0]] << like[1]
+            else
+              @likes_for_explore_athletics[like[0]] = [like[1]]
             end
           end # end announcement likes iteration
         end # end scores blank if else
