24f070324da48c7a881d691f7889a51a55a0892a
kujenga <ataylor0123@gmail.com>
2014-08-26 00:23:40 -0400

    fixed model display name for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -133,7 +133,7 @@ class User < ActiveRecord::Base
 
   # for active admin
   def display_name
-    user.email.blank? ? user.id : "#{first_name} #{last_name}"
+    email.blank? ? id : "#{first_name} #{last_name}"
   end
 
   ###############################
