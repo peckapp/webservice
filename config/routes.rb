536bce0397d7e1f25f19f42547a8ac4f908b3367
kujenga <ataylor0123@gmail.com>
2014-08-26 16:39:33 -0400

    updated routes for updating device tokens and began traversing nested scraper

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
       resources(:activity_logs, :athletic_events, :athletic_teams, :clubs, :circles, :configurations,
                 :departments, :dining_opportunities, :dining_periods, :dining_places, :views,
                 :events_page_urls, :institutions, :locations, :menu_items, :notification_views,
-                :pecks, :explore, :unique_device_identifiers)
+                :pecks, :explore)
 
       resources :access, only: [:create] do
         collection do
@@ -112,6 +112,7 @@ Rails.application.routes.draw do
       # custom route for updating the device token associated with a udid
       patch 'unique_device_identifiers/update_token', to: 'unique_device_identifiers#update_token'
 
+      resources :unique_device_identifiers
     end
 
     scope module: :v2, constraints: ApiConstraints.new(version: 2) do
