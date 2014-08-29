980f67001ebb94e398857ee7e0a73595dce7f6b1
kujenga <ataylor0123@gmail.com>
2014-08-29 16:04:14 -0400

    commented out broken image content code

--- app/models/concerns/image_content.rb
+++ app/models/concerns/image_content.rb
@@ -9,24 +9,24 @@ module ImageContent
   # *** Must define method 'image_path_root' in subclasses with unique path start for S3 storage ***
 
   ### Default home photo attachments ###
-  has_attached_file(:image,
-                    s3_credentials: {
-                      bucket: ENV['AWS_BUCKET_NAME'],
-                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-                    },
-                    # path: ':rails_root/public/images/simple_events/:style/:basename.:extension',
-                    url: "/images/#{root}/:style/:basename.:extension",
-                    default_url: '/images/missing.png',
-                    path: "images/#{root}/:style/:basename.:extension",
-                    styles: {
-                      detail: '100X100#',
-                      blurred: {
-                        size: '640x256',
-                        offset: '+0+0',
-                        raduis_sigma: '9x4',
-                        tint: '40',
-                        processors: [:blur]
-                      }
-                    })
+  # has_attached_file(:image,
+  #                   s3_credentials: {
+  #                     bucket: ENV['AWS_BUCKET_NAME'],
+  #                     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #                     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  #                   },
+  #                   # path: ':rails_root/public/images/simple_events/:style/:basename.:extension',
+  #                   url: "/images/#{root}/:style/:basename.:extension",
+  #                   default_url: '/images/missing.png',
+  #                   path: "images/#{root}/:style/:basename.:extension",
+  #                   styles: {
+  #                     detail: '100X100#',
+  #                     blurred: {
+  #                       size: '640x256',
+  #                       offset: '+0+0',
+  #                       raduis_sigma: '9x4',
+  #                       tint: '40',
+  #                       processors: [:blur]
+  #                     }
+  #                   })
 end
