bf629a6b51005ba7b52afd230f8a283ef712d181
kujenga <ataylor0123@gmail.com>
2014-08-30 13:08:48 -0400

    moved image content code back into a model that is specified as abstract, working for athletic teams

---
+++ app/models/image_content_model.rb
@@ -0,0 +1,37 @@
+#### NOT A REAL MODEL, ONLY TO BE SUBCLASSED!!! ####
+
+# not working for some reason, stack overflow question pending on it
+
+# superclass of all models with common paperclip settings for images
+class ImageContentModel < ActiveRecord::Base
+  # prevents active admin from
+  self.abstract_class = true
+
+  # *** Must define method 'image_path_root' in subclasses with unique path start for S3 storage ***
+
+  ### Default home photo attachments ###
+  has_attached_file(:image,
+                    s3_credentials: {
+                      bucket: ENV['AWS_BUCKET_NAME'],
+                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+                    },
+                    # path: ':rails_root/public/images/simple_events/:style/:basename.:extension',
+                    url: "/images/#{@image_path_root}/:style/:basename.:extension",
+                    default_url: '/images/missing.png',
+                    path: "images/#{@image_path_root}/:style/:basename.:extension",
+                    styles: {
+                      detail: '100X100#',
+                      blurred: {
+                        size: '640x256',
+                        offset: '+0+0',
+                        raduis_sigma: '9x4',
+                        tint: '40',
+                        processors: [:blur]
+                      }
+                    })
+
+  # validates_attachment :image, :content_type => { :content_type => "image/jpeg"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 5.megabytes
+end
