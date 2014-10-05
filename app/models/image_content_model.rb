84fb4f0acffd7ec0488f817e025dfe63796dd334
kujenga <ataylor0123@gmail.com>
2014-10-05 12:54:56 +0200

    temporarily fixed issue with asset precompilation with imagecontentmodel

--- app/models/image_content_model.rb
+++ app/models/image_content_model.rb
@@ -13,11 +13,25 @@ class ImageContentModel < ActiveRecord::Base
   def self.attach_file_with_root(root)
     # sets up paperclip model
     has_attached_file(:image,
-                      s3_credentials: defined_s3_credentials,
+                      s3_credentials: {
+                        bucket: ENV['AWS_BUCKET_NAME'],
+                        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+                      },
                       url: "/images/#{root}/:style/:basename.:extension",
                       default_url: '/images/missing.png',
                       path: "images/#{root}/:style/:basename.:extension",
-                      styles: defined_styles)
+                      styles: {
+                        detail: '100X100#',
+                        blurred: {
+                          size: '640x256',
+                          offset: '+0+0',
+                          raduis_sigma: '9x4',
+                          tint: '40',
+                          processors: [:blur]
+                        },
+                        home: '640X640#'
+                      })
 
     # validates_attachment :image, :content_type => { :content_type => "image/jpeg"}
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
@@ -26,6 +40,7 @@ class ImageContentModel < ActiveRecord::Base
 
   protected
 
+  # breaks asset precompilation for some reason
   def defined_styles
     default_styles = {
       detail: '100X100#',
@@ -39,13 +54,5 @@ class ImageContentModel < ActiveRecord::Base
       home: '640X640#'
     }
     default_styles
-  end
-
-  def defined_s3_credentials
-    {
-      bucket: ENV['AWS_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
   end
 end
