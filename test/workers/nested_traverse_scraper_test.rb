d443a79575f1534cf78a0b105272ad2c721242e9
kujenga <ataylor0123@gmail.com>
2014-10-11 22:51:21 +0200

    outlining tests for the nested traverse scraper

--- test/workers/nested_traverse_scraper_test.rb
+++ test/workers/nested_traverse_scraper_test.rb
@@ -7,4 +7,16 @@ class NestedTraverseScraperTest < ActionController::TestCase
 
   def teardown
   end
+
+  # test methods handling elements with a sample page
+  # test handle_element
+  # test handle_content_element
+  # test handle_foreign_key_element
+
+  # methods handling idempotent saving operations
+  # test validate_and_save
+  # test idempotent_save
+  # test closest_match
+  # test model_crucial_params
+  # test model_match_params
 end
