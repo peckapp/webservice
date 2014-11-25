fc361bda13e82ad957df7a8bc140fa64ae6b4f00
kujenga <ataylor0123@gmail.com>
2014-11-25 11:59:27 +0100

    added Procfile and updated Dalli Client for heroku addon

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec unicorn -p $PORT -c ./config/unicorn/production.rb
+worker: bundle exec sidekiq -c 5 -v
--- lib/peck_dalli.rb
+++ lib/peck_dalli.rb
@@ -4,8 +4,15 @@ class PeckDalli
     options = { namespace: 'peck', compress: true }
 
     if Rails.env.production?
-      # magni only accepts memcahced requests on DO private IP address
-      return Dalli::Client.new('10.128.132.85:11211', options)
+      if ENV['MEMCACHEDCLOUD_SERVERS']
+        return Dalli::Client.new(ENV['MEMCACHEDCLOUD_SERVERS'], username: ENV['MEMCACHEDCLOUD_USERNAME'],
+                                                                password: ENV['MEMCACHEDCLOUD_PASSWORD'],
+                                                                namespace: 'peck',
+                                                                compress: true)
+      else
+        # magni only accepts memcached requests on DO private IP address
+        return Dalli::Client.new('10.128.132.85:11211', options)
+      end
     elsif Rails.env.staging?
       # should be different from production
       return nil # Dalli::Client.new('magni.peckapp.com:11211', options)
